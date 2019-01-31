26bce03b664d7451f4f40aaa2a9c043b4fb4952d
Zack Bostian <bostianz@gmail.com>
2019-01-31 11:37:43 -0500

    updated project with server files

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID: '247307331806-tj1oci0u1qrso889ohnqpoebmp9tdgtg.apps.googleusercontent.com',
+  googleClientSecret: 'JC7oosbrrMw-ITo4blyjtL8P',
+  mongoURI: 'mongodb://bostianz:ai8fww6t89@ds117545.mlab.com:17545/budgety-dev'
+}
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
