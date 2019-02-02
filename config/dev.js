547de781d6f73ea630794fc7ce78b3e17f549945
Zack Bostian <bostianz@gmail.com>
2019-02-02 10:35:24 -0500

    tweaked google strategy settings

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '247307331806-tj1oci0u1qrso889ohnqpoebmp9tdgtg.apps.googleusercontent.com',
+  googleClientSecret: 'JC7oosbrrMw-ITo4blyjtL8P',
+  mongoURI: 'mongodb://bostianz:ai8fww6t89@ds117545.mlab.com:17545/budgety-dev',
+  cookieKey: 'jkagsd78613t8hgb2jbf8972g7982b4uo028h9t71gbu'
+};
--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-  googleClientID: '247307331806-tj1oci0u1qrso889ohnqpoebmp9tdgtg.apps.googleusercontent.com',
-  googleClientSecret: 'JC7oosbrrMw-ITo4blyjtL8P',
-  mongoURI: 'mongodb://bostianz:ai8fww6t89@ds117545.mlab.com:17545/budgety-dev',
-  cookieKey: 'jkagsd78613t8hgb2jbf8972g7982b4uo028h9t71gbu'
+if (process.env.NODE === 'production') {
+  module.exports = require('./prod');
+} else {
+  module.exports = require('./dev');
 }
